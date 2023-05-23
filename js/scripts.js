b6842ea1491e20531f0fa30d7ce9de6672eeb7c0
viniciussouza88 <souza.vinicius.88@gmail.com>
2023-05-22 23:14:44 -0300

    Menu mobile do JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log('Oie!!!!')
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+[menuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-active")
+    })
+}) 
