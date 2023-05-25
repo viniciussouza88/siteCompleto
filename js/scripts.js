0c4634b1b11e501ac79a38888d729cea41c1fc17
viniciussouza88 <souza.vinicius.88@gmail.com>
2023-05-24 23:36:44 -0300

    Menu de ancoragem (Smoth Scroll)

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,43 @@
+// Seleção de elementos
+
 const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
+
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+// Funções
+
+function smoothScroll(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    }
+    );
+
+    setTimeout (() => {
+        if (menu.classList.contains("#menu-active")) {
+            menu.classList.remove("menu-active");
+        }
+    }, 500);
+}
+
+// Eventos 
 
 [menuBtn, closeMenuBtn].forEach((btn) => {
     btn.addEventListener("click", (e) => {
         menu.classList.toggle("menu-active")
     })
 }) 
+
+allLinks.forEach((link) => {
+   link.addEventListener("click", smoothScroll);
+});
+
